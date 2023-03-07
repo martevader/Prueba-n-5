d433989c7573cc24c59e44e0f56b9bc4ce32bef3
martevader <127197820+martevader@users.noreply.github.com>
2023-03-07 12:01:03 +0100

    Prueba 5

---
+++ Prueba 5/Prueba 5.R
@@ -0,0 +1,7 @@
+#Ejercicio 1. Crear vector
+numArtefactos <- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
+typeof(numArtefactos)
+as.integer(numArtefactos)
+is.integer(numArtefactos)
+
+#Ejercicio 2. Hacer la media
